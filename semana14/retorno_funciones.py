2bc5d8230de0c467301b9727da05636f70cc94b0
capodev <fr.jimenezv@uea.edu.ec>
2024-09-16 15:27:48 -0500

    Update retorno_funciones.py
    se realizo los comentarios que me habia olvidado

--- semana14/retorno_funciones.py
+++ semana14/retorno_funciones.py
@@ -7,10 +7,12 @@ def calcular_descuento(monto_total, descuento):
     final_price = monto_total - monto_descuento
     return "El total es de: ", final_price, " El descuento seria de: ", monto_descuento
 
-
+#realizamos un do while para hacer multiples intentos, manipulado por el usuario
 while True:
+    #ingresamos los valores por teclado
     monto = float(input("Ingrese el monto total: "))
     descuentos = float(input("Ingrese el descuento: "))
+    #llamamos a la funcion calcular_decuento que nos devolvera un string el cual contiene los valores rrequeridos
     print(calcular_descuento(monto, descuentos))
     if input("Desea calcular otro descuento? (s/n): ") == "n":
         break
